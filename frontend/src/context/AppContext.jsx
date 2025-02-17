df4e732f1682ebef073fcb704f903783108343c7
TriNM2003 <133070397+TriNM2003@users.noreply.github.com>
2025-02-17 22:10:46 +0700

    fix AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -21,6 +21,7 @@ const AppProvider = ({ children }) => {
     // },[])
     // api
     const authAPI = "http://localhost:9999/auth";
+    const userApi = "http://localhost:9999/users";
 
       // State lưu thông tin user & accessToken
   
@@ -32,9 +33,25 @@ const AppProvider = ({ children }) => {
 
 
 
-    //call api
 
+    //call api
+    useEffect(() => {
+       
+            
+    });
     //fuction
+    const changePassword = async (userId, oldPassword, newPassword) => {
+        try {
+            const response = await axios.put(`${userApi}/change-password`, {
+                userId,
+                oldPassword,
+                newPassword
+            });
+            return response.data;
+        } catch (error) {
+            throw error.response.data;
+        }
+    };
 
     return (
         <AppContext.Provider value={{
