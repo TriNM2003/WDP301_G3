5b299c89e731d75c61ca2c050b09f458a7ead51d
TriNM <nguyentri16112003@gmail.com>
2025-02-21 10:45:11 +0700

    feat project summary

--- frontend/src/components/Project/Detail/Summary.jsx
+++ frontend/src/components/Project/Detail/Summary.jsx
@@ -1,61 +1,270 @@
-import { ArrowUpOutlined, DollarCircleOutlined, PlusCircleFilled, PlusCircleOutlined } from '@ant-design/icons'
-import { Card, Col, Row, Statistic } from 'antd'
+import { blue, green, grey, orange, red } from '@ant-design/colors'
+import { ArrowUpOutlined, BugOutlined, CheckCircleOutlined, CloseCircleOutlined, DollarCircleOutlined, ExclamationCircleOutlined, FireFilled, FireOutlined, FireTwoTone, FormOutlined, PaperClipOutlined, PlusCircleFilled, PlusCircleOutlined, ProfileOutlined } from '@ant-design/icons'
+import { Avatar, Card, Col, Flex, Progress, Row, Space, Statistic } from 'antd'
+import { Bar, BarChart, Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import { icons } from 'antd/es/image/PreviewGroup'
 import React from 'react'
+import Title from 'antd/es/typography/Title'
+
 
 function Summary() {
+
+  const data = [
+    { name: "To Do", value: 48, color: "#d86fc5" },
+    { name: "In Reviewing", value: 0, color: "#2a61dd" },
+    { name: "In Progress", value: 16, color: "#db6b0a" },
+    { name: "Done", value: 113, color: "#5b9215" },
+  ];
+  const priority = [
+    { name: "To Do", value: 48, icon: <ExclamationCircleOutlined /> },
+    { name: "In Reviewing", value: 0, icon: <ExclamationCircleOutlined /> },
+    { name: "In Progress", value: 16, icon: <ExclamationCircleOutlined /> },
+    { name: "Done", value: 113, icon: <ExclamationCircleOutlined /> },
+  ];
   return (
     <div style={{ padding: "2% 5%" }}>
+
       <Row style={{ padding: "2% 0" }} justify="space-between">
         <Col span={5}>
-          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" , textAlign:"start"}}>
+          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", textAlign: "start" }}>
             <Statistic
-              title="Total activities"
+              title="Created"
               value="500"
-              valueStyle={{ color: "#3f8600" }}
-              prefix={<DollarCircleOutlined />}
+              // valueStyle={{ color: blue.primary }}
+              prefix={<ProfileOutlined style={{ color: blue.primary }} />}
             />
-            <p>+20 from last week</p>
+            <p style={{ margin: 0 }}>in the last week</p>
           </Card>
         </Col>
+
         <Col span={5}>
-          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", textAlign:"start" }}>
+          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", textAlign: "start" }}>
             <Statistic
               title="Completed"
-              value="200"
-              valueStyle={{ color: "#3f8600" }}
-              prefix={<ArrowUpOutlined />}
+              value="500"
+              // valueStyle={{ color: green.primary }}
+              prefix={<CheckCircleOutlined style={{ color: green.primary }} />}
             />
-            <p>in last week</p>
+            <p style={{ margin: 0 }}>in the last week</p>
+          </Card>
+        </Col>
+
+        <Col span={5}>
+          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", textAlign: "start" }}>
+            <Statistic
+              title="Bug"
+              value="10"
+              // valueStyle={{ color: orange[6] }}
+              prefix={<BugOutlined style={{ color: orange[6] }} />}
+            />
+            <p style={{ margin: 0 }}>in the last week</p>
           </Card>
         </Col>
         <Col span={5}>
-          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" , textAlign:"start"}}>
+          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", textAlign: "start" }} >
             <Statistic
-              title="Total Revenue"
-              value="$45,231.89"
-              valueStyle={{ color: "#3f8600" }}
-              prefix={<DollarCircleOutlined />}
+              title="Overdue"
+              value="10"
+              // valueStyle={{ color: red[6] }}
+              prefix={<CloseCircleOutlined style={{ color: red[6] }} />}
             />
-            <p>+20.1% from last month</p>
-          </Card>
-        </Col>
-        <Col span={5}>
-          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" , textAlign:"start"}}>
-            <Statistic
-              title="Total Revenue"
-              value="$45,231.89"
-              valueStyle={{ color: "#3f8600" }}
-              prefix={<DollarCircleOutlined />}
-            />
-            <p>+20.1% from last month</p>
+            <p style={{ margin: 0 }}>in the next week</p>
           </Card>
         </Col>
       </Row>
       <Row style={{ padding: "2% 0" }} justify="space-between">
-        <Col span={5}>1</Col>
-        <Col span={5}>1</Col>
-        <Col span={5}>1</Col>
+        <Col span={11}>
+          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", overflow: "auto", height: "400px", textAlign: "start" }} >
 
+            <Title level={4} style={{ margin: 0 }} >Activity Summary</Title>
+            <small style={{ color: grey[4] }}><em>Quickly review the progress of your ongoing activities.</em></small>
+            <ResponsiveContainer width="80%" height={300}>
+              <PieChart>
+                <Pie
+                  data={data}
+                  dataKey="value"
+                  nameKey="name"
+                  cx="50%"
+                  cy="50%"
+                  innerRadius={65}
+                  outerRadius={90}
+                  fill="#8884d8"
+                  label
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={entry.color} />
+                  ))}
+
+                </Pie>
+                <Tooltip />
+                <Legend
+                  layout="vertical"
+                  align="right"
+                  verticalAlign="middle"
+                />
+                <text x="38%" y="45%" textAnchor="middle" dominantBaseline="middle" fontSize={24} fontWeight="bold">
+                  122
+                </text>
+                <text x="38%" y="55%" textAnchor="middle" dominantBaseline="middle" fontSize={14} >
+                  Total Issues
+                </text>
+              </PieChart>
+            </ResponsiveContainer>
+          </Card>
+
+        </Col>
+
+        <Col span={11} >
+          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", overflow: "auto", height: "400px", paddingBottom: "5%", textAlign: "start" }}  >
+            <Title level={4} style={{ margin: 0 }} >Project Capacity Overview</Title>
+            <small style={{ color: grey[4] }}><em>Track your project's workload and availability.</em></small>
+            <Flex style={{ margin: "2% 0" }} justify='space-between' align='start'>
+              <Col span={4}>
+                <small style={{ fontWeight: "bolder", color: grey[4] }}>Member</small>
+              </Col>
+              <Col span={18} align="start">
+                <small style={{ fontWeight: "bolder", color: grey[4] }}>Work distribution</small>
+
+              </Col>
+            </Flex>
+            <Flex style={{ margin: "2% 0" }} justify='space-between' align='start'>
+              <Col span={4}>
+                <Space>
+                  <Avatar src={<img src="https://i.pinimg.com/736x/49/9c/5e/499c5e44dcb6bc40bcf47cd3d6d1fdf0.jpg" alt="avatar" />} />
+                  <text>TriNM</text>
+                </Space>
+              </Col>
+              <Col span={18}>
+                <Progress
+
+                  percent={65}
+                  percentPosition={{
+                    type: 'inner',
+                    align: 'end'
+
+                  }}
+                  strokeLinecap="square"
+                  size={["100%", 25]}
+                  strokeColor={green[3]}
+                />
+              </Col>
+            </Flex>
+
+            <Flex style={{ margin: "2% 0" }} justify='space-between' align='start'>
+              <Col span={4}>
+                <Space>
+                  <Avatar src={<img src="https://i.pinimg.com/736x/49/9c/5e/499c5e44dcb6bc40bcf47cd3d6d1fdf0.jpg" alt="avatar" />} />
+                  <text>TriNM</text>
+                </Space>
+              </Col>
+              <Col span={18}>
+                <Progress
+
+                  percent={65}
+                  percentPosition={{
+                    type: 'inner',
+                    align: 'end'
+
+                  }}
+                  strokeLinecap="square"
+                  size={["100%", 25]}
+                  strokeColor={green[3]}
+                />
+              </Col>
+            </Flex>
+
+
+          </Card>
+        </Col>
+
+      </Row>
+      <Row style={{ padding: "2% 0" }} justify="space-between">
+        <Col span={11}>
+          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", overflow: "auto", height: "400px", paddingBottom: "5%", textAlign: "start" }}   >
+            <Title level={4} style={{ margin: 0 }} >Task Prioritization Overview</Title>
+            <small style={{ color: grey[4] }}><em>Gain insight into how tasks are being ranked in importance.</em></small>
+
+            <ResponsiveContainer width="100%" height={250} style={{ marginTop: "2%" }}>
+              <BarChart data={data} >
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+
+                {/* Chỉ dùng 1 <Bar> với dataKey="value" */}
+                <Bar dataKey="value" name="Priority Level" fill={grey[0]}>
+                  {data.map((entry, index) => (
+                    <Bar key={index} dataKey="value" />
+                  ))}
+                </Bar>
+              </BarChart>
+            </ResponsiveContainer>
+
+
+          </Card>
+        </Col>
+        <Col span={11}>
+          <Card style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", overflow: "auto", height: "400px", paddingBottom: "5%", textAlign: "start" }}   >
+            <Title level={4} style={{ margin: 0 }} >Types of work</Title>
+            <small style={{ color: grey[4] }}><em>Get a breakdown of issues by their types.</em></small>
+
+            <Flex style={{ margin: "2% 0" }} justify='space-between' align='start'>
+              <Col span={4}>
+                <small style={{ fontWeight: "bolder", color: grey[4] }}>Type</small>
+              </Col>
+              <Col span={18} align="start">
+                <small style={{ fontWeight: "bolder", color: grey[4] }}>Distribution</small>
+
+              </Col>
+            </Flex>
+            <Flex style={{ margin: "2% 0" }} justify='space-between' align='start'>
+              <Col span={4}>
+                <Space>
+                  <FormOutlined color={blue.primary} />
+                  <text>Task</text>
+                </Space>
+              </Col>
+              <Col span={18}>
+                <Progress
+
+                  percent={65}
+                  percentPosition={{
+                    type: 'inner',
+                    align: 'end'
+
+                  }}
+                  strokeLinecap="square"
+                  size={["100%", 25]}
+                  strokeColor={green[3]}
+                />
+              </Col>
+            </Flex>
+            <Flex style={{ margin: "2% 0" }} justify='space-between' align='start'>
+              <Col span={4}>
+                <Space>
+                  <PaperClipOutlined />
+                  <text>Subtask</text>
+                </Space>
+              </Col>
+              <Col span={18}>
+                <Progress
+
+                  percent={65}
+                  percentPosition={{
+                    type: 'inner',
+                    align: 'end'
+
+                  }}
+                  strokeLinecap="square"
+                  size={["100%", 25]}
+                  strokeColor={green[3]}
+                />
+              </Col>
+            </Flex>
+
+
+
+          </Card>
+        </Col>
       </Row>
     </div>
   )
--- frontend/src/pages/Sites/_id.jsx
+++ frontend/src/pages/Sites/_id.jsx
@@ -4,12 +4,14 @@ import Sider from 'antd/es/layout/Sider'
 import { Content } from 'antd/es/layout/layout'
 import React from 'react'
 import SiteSider from '../../components/Site/Layout/SiteSider'
-import SiteContent from '../../components/Site/Layout/SiteContent'
+import SiteContent from '../../components/Site/Layout/SiteContent'  
 
 function _id() {
     return (
-        <Layout style={{ height: 'calc(100vh - 64px)', }}>
-            <Sider style={{
+        <Layout style={{ height: 'calc(100vh - 64px)' }}>
+            <Sider
+                width={"15%"}
+             style={{
                 height: '100%',
                 background: "white",
                 borderRight: `solid 1px ${cyan[`1`]}`
