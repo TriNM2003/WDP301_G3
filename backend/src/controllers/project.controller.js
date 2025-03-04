85fbc4eebdc7e7f3713aafcc8f227308f354abac
HaoTH <haothhe176192@fpt.edu.vn>
2025-03-04 20:55:16 +0700

    WG-194<feat:(BE)view list project>

--- backend/src/controllers/project.controller.js
+++ backend/src/controllers/project.controller.js
@@ -5,6 +5,18 @@ const morgan = require("morgan")
 const createHttpErrors = require("http-errors");
 const projectService = require('../services/project.service');
 
+const getAllProjects = async (req, res, next) => {
+    try {
+      
+        const project = await projectService.getAllProjects()
+        if (!project) {
+            return res.status(404).json({ error: { status: 404, message: "Project not found" } })
+        }
+        res.status(200).json(project);
+    } catch (error) {
+        next(error);
+    }
+}
 
 const getProjectById = async (req, res, next) => {
     try {
@@ -20,7 +32,8 @@ const getProjectById = async (req, res, next) => {
 }
 
 const projectController = {
-    getProjectById
+    getProjectById,
+    getAllProjects
 }
 
 module.exports = projectController;
