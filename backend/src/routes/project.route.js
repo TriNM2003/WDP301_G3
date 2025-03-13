43238a8a4a928f273a688bf053d5e46cd17b4ad8
lehoan411 <notlehoan411@gmail.com>
2025-03-13 21:22:32 +0700

    feat(be: fix route)

--- backend/src/routes/project.route.js
+++ backend/src/routes/project.route.js
@@ -52,14 +52,12 @@ projectRouter.put("/:projectId/project-setting",
     authMiddleware.verifyAccessToken,
     projectMiddleware.isInProject,
     projectMiddleware.isProjectManager,
-    siteMiddleware.isSiteOwner,
     cloudinary.upload.single("projectAvatar"),
     projectController.editProject
 )
 projectRouter.put("/:projectId/remove-to-trash",
     authMiddleware.verifyAccessToken,
     projectMiddleware.isInProject,
-    siteMiddleware.isSiteOwner,
     projectMiddleware.isProjectManager,
     projectController.removeToTrash
 );
@@ -68,7 +66,6 @@ projectRouter.get("/trash",
     authMiddleware.verifyAccessToken,
     projectMiddleware.isInProject,
     projectMiddleware.isProjectManager,
-    siteMiddleware.isSiteOwner,
     projectController.getProjectTrash
 );
 
@@ -76,7 +73,6 @@ projectRouter.put("/:projectId/restore",
     authMiddleware.verifyAccessToken,
     projectMiddleware.isInProject,
     projectMiddleware.isProjectManager,
-    siteMiddleware.isSiteOwner,
     projectController.restoreProject
 );
 
@@ -84,7 +80,6 @@ projectRouter.delete("/:projectId/destroy",
     authMiddleware.verifyAccessToken,
     projectMiddleware.isInProject,
     projectMiddleware.isProjectManager,
-    siteMiddleware.isSiteOwner,
     projectController.destroyProject
 );
 
