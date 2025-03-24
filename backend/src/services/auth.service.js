a2c9e67a1241e8f194a5bb7bdef3603c8593e9ec
hungambrose123 <hungambrose123@gmail.com>
2025-03-24 21:21:39 +0700

    hotfix: login by google

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -152,11 +152,6 @@ const getUserByAccessToken = async (accessToken) => {
 
     }
 
-    // refresh token
-    const refreshToken = jwtUtils.generateRefreshToken(user._id);
-    // luu vao trong redis
-    await redisClient.setRefreshToken(user._id, refreshToken, jwtUtils.refreshTokenExp);
-
     return {
         _id: user._id,
         username: user.username,
