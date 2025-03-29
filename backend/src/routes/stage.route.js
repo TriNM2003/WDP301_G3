98c6702ef7bac0ef7b48feeaa57a91f54277ce4e
HaoTH <haothhe176192@fpt.edu.vn>
2025-03-29 16:05:38 +0700

    fix:drag and drop stage

--- backend/src/routes/stage.route.js
+++ backend/src/routes/stage.route.js
@@ -12,19 +12,19 @@ stageRouter.get("/get-all",
     stageController.getStagesByProjectId
 )
 stageRouter.post("/update-parents",
-    [authMiddleware.verifyAccessToken,accountMiddleware.isActive,siteMiddleware.isInSite, projectMiddleware.isInProject],
+    [authMiddleware.verifyAccessToken,accountMiddleware.isActive,siteMiddleware.isInSite, projectMiddleware.isInProject, projectMiddleware.isProjectManager],
     stageController.updateStageParents
 )
 stageRouter.post("/add",
-    [authMiddleware.verifyAccessToken,accountMiddleware.isActive,siteMiddleware.isInSite, projectMiddleware.isInProject],
+    [authMiddleware.verifyAccessToken,accountMiddleware.isActive,siteMiddleware.isInSite, projectMiddleware.isInProject, projectMiddleware.isProjectManager],
     stageController.addStage
 )
 stageRouter.post("/update",
-    [authMiddleware.verifyAccessToken,accountMiddleware.isActive,siteMiddleware.isInSite, projectMiddleware.isInProject],
+    [authMiddleware.verifyAccessToken,accountMiddleware.isActive,siteMiddleware.isInSite, projectMiddleware.isInProject, projectMiddleware.isProjectManager],
     stageController.updateStage
 )
 stageRouter.post("/delete",
-    [authMiddleware.verifyAccessToken,accountMiddleware.isActive,siteMiddleware.isInSite, projectMiddleware.isInProject],
+    [authMiddleware.verifyAccessToken,accountMiddleware.isActive,siteMiddleware.isInSite, projectMiddleware.isInProject, projectMiddleware.isProjectManager],
     stageController.deleteStage
 )
 
